2731ba860c46d6f4f3b1932e3dc74472406e4619
Vladislav <vladaksenov31073@gmail.com>
2025-01-23 20:00:11 +1000

    Добавьте файлы проекта.

---
+++ asp.net/Controllers/GreetController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SimpleMvcApp.Controllers
+{
+    public class GreetController : Controller
+    {
+        public IActionResult Index(string name)
+        {
+            ViewData["Name"] = name;
+            return View();
+        }
+    }
+}
---
+++ asp.net/HomeController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+public class PageController : Controller
+{
+    public IActionResult Welcome()
+    {
+        return View();
+    }
+}
